4fd0e265c9384fc5468b0407d1ef4aa9611842bf
shubhhampgit01 <shubhhampgit01@gmail.com>
2024-07-22 18:51:05 +0530

    testOne

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import HomePage from './lib/Home/HomePage'
+const App = () => {
+  return (
+    <div>
+      <h1>Main container</h1>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/lib/Home/HomePage.tsx
@@ -0,0 +1,13 @@
+
+
+function HomePage() {
+    return (
+        <div>
+            <h1>
+                hello home page
+            </h1>
+        </div>
+    )
+}
+
+export default HomePage
